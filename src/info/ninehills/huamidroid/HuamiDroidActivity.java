ddb04af62d052082a518fa81963689ce6e210d55
Tao Yang <swulling@gmail.com>
2012-01-02 14:38:55 +0800

    Init android project.

---
+++ src/info/ninehills/huamidroid/HuamiDroidActivity.java
@@ -0,0 +1,13 @@
+package info.ninehills.huamidroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HuamiDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
