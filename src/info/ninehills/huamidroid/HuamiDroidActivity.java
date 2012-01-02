ad15f84ce7128472bdc8a05e25898a45911cf8be
Tao Yang <swulling@gmail.com>
2012-01-02 15:04:39 +0800

    Use phonegap framework

--- src/info/ninehills/huamidroid/HuamiDroidActivity.java
+++ src/info/ninehills/huamidroid/HuamiDroidActivity.java
@@ -1,13 +1,13 @@
 package info.ninehills.huamidroid;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.phonegap.*;
 
-public class HuamiDroidActivity extends Activity {
+public class HuamiDroidActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
